151c8c4c0a4574fa3f4a52cdc2af9e5efa41cc85
Alexis Kofman <alexis.kofman@gmail.com>
2015-01-23 11:34:21 +0100

    Add options attribute to configure Hammer manager

--- src/Hammer.js
+++ src/Hammer.js
@@ -1,17 +1,17 @@
 var React = require('react'),
-	Hammer = require('hammerjs');
+    Hammer = require('hammerjs');
 
 var privateProps = {
-	component: true,
-	children: true,
-	action: true,
-	onTap: true,
-	onDoubleTap: true,
-	onPan: true,
-	onSwipe: true,
-	onPress: true,
-	onPinch: true,
-	onRotate: true
+    component: true,
+    children: true,
+    action: true,
+    onTap: true,
+    onDoubleTap: true,
+    onPan: true,
+    onSwipe: true,
+    onPress: true,
+    onPinch: true,
+    onRotate: true
 };
 
 /**
@@ -20,51 +20,69 @@ var privateProps = {
  */
 
 var HammerComponent = React.createClass({
-	
-	displayName: 'Hammer',
-	
-	propTypes: {
-		component: React.PropTypes.any,
-		className: React.PropTypes.string
-	},
-	
-	getDefaultProps: function() {
-		return {
-			component: 'span'
-		};
-	},
-	
-	componentDidMount: function() {
-		this.hammer = new Hammer(this.getDOMNode());
-		if (this.props.action)		this.hammer.on('tap press', 	this.props.action);
-		if (this.props.onTap)		this.hammer.on('tap',			this.props.onTap);
-		if (this.props.onDoubleTap)	this.hammer.on('doubletap',		this.props.onDoubleTap);
-		if (this.props.onPan)		this.hammer.on('pan',			this.props.onPan);
-		if (this.props.onSwipe)		this.hammer.on('swipe',			this.props.onSwipe);
-		if (this.props.onPress)		this.hammer.on('press',			this.props.onPress);
-		if (this.props.onPinch)		this.hammer.on('pinch',			this.props.onPinch);
-		if (this.props.onRotate)	this.hammer.on('rotate',		this.props.onRotate);
-	},
-	
-	componentWillUnmount: function() {
-		this.hammer.stop();
-		this.hammer.destroy();
-		this.hammer = null;
-	},
-	
-	render: function() {
-		
-		var props = {};
-		
-		Object.keys(this.props).forEach(function(i) {
-			if (!privateProps[i]) {
-				props[i] = this.props[i];
-			}
-		}, this);
 
-		return React.createElement(this.props.component, props, this.props.children);
-	}
-	
+    displayName: 'Hammer',
+
+    propTypes: {
+        component: React.PropTypes.any,
+        className: React.PropTypes.string,
+        options: React.PropTypes.object
+    },
+
+    getDefaultProps: function() {
+        return {
+            component: 'span'
+        };
+    },
+
+    componentDidMount: function() {
+        this.hammer = new Hammer(this.getDOMNode());
+
+        if (this.props.options) {
+            Object.keys(this.props.options).forEach(function(option) {
+                if (option === 'recognizers') {
+                    Object.keys(this.props.options.recognizers).forEach(function(gesture) {
+                        var recognizer = this.hammer.get(gesture);
+                        recognizer.set(this.props.options.recognizers[gesture]);
+                    }, this);
+                } else {
+                    var key = option;
+                    var optionObj = {};
+                    optionObj[key] = this.props.options[option];
+                    this.hammer.set(optionObj);
+                }
+            }, this);
+        }
+
+        if (this.props.action) this.hammer.on('tap press', this.props.action);
+        if (this.props.onTap) this.hammer.on('tap', this.props.onTap);
+        if (this.props.onDoubleTap) this.hammer.on('doubletap', this.props.onDoubleTap);
+        if (this.props.onPan) this.hammer.on('pan', this.props.onPan);
+        if (this.props.onSwipe) this.hammer.on('swipe', this.props.onSwipe);
+        if (this.props.onPress) this.hammer.on('press', this.props.onPress);
+        if (this.props.onPinch) this.hammer.on('pinch', this.props.onPinch);
+        if (this.props.onRotate) this.hammer.on('rotate', this.props.onRotate);
+    },
+
+    componentWillUnmount: function() {
+        this.hammer.stop();
+        this.hammer.destroy();
+        this.hammer = null;
+    },
+
+    render: function() {
+
+        var props = {};
+
+        Object.keys(this.props).forEach(function(i) {
+            if (!privateProps[i]) {
+                props[i] = this.props[i];
+            }
+        }, this);
+
+        return React.createElement(this.props.component, props, this.props.children);
+    }
+
 });
 
 module.exports = HammerComponent;
