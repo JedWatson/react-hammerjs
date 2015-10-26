fd5e08605c08fb4995e0a5b1e8493fa949a1aff7
Eduardo De Lisa <eduardo@deplabs.com>
2015-10-26 15:27:15 -0300

    add Pinch In and Out events
    add Pinch In and Out events

--- src/Hammer.js
+++ src/Hammer.js
@@ -17,6 +17,8 @@ var privateProps = {
 	onPress: true,
 	onPressUp: true,
 	onPinch: true,
+	onPinchIn: true,
+	onPinchOut: true,
 	onRotate: true
 };
 
@@ -67,6 +69,8 @@ var HammerComponent = React.createClass({
 		if (this.props.onPress) this.hammer.on('press', this.props.onPress);
 		if (this.props.onPressUp) this.hammer.on('pressup', this.props.onPressUp);
 		if (this.props.onPinch) this.hammer.on('pinch', this.props.onPinch);
+		if (this.props.onPinchIn) this.hammer.on('pinchin', this.props.onPinchIn);
+		if (this.props.onPinchOut) this.hammer.on('pinchout', this.props.onPinchOut);
 		if (this.props.onRotate) this.hammer.on('rotate', this.props.onRotate);
 	},
 
