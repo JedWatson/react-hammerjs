845afc542f060e46e553b382edaefc0b180739f8
Jed Watson <jed.watson@me.com>
2017-10-25 00:02:55 +1100

    Format with prettier

--- src/Hammer.js
+++ src/Hammer.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 
 // require('hammerjs') when in a browser. This is safe because Hammer is only
 // invoked in componentDidMount, which is not executed on the server.
-const Hammer = (typeof window !== 'undefined') ? require('hammerjs') : undefined;
+const Hammer = typeof window !== 'undefined' ? require('hammerjs') : undefined;
 
 var privateProps = {
 	children: true,
@@ -47,30 +47,34 @@ const handlerToEvent = {
 	onTap: 'tap',
 };
 
-Object.keys(handlerToEvent).forEach(function (i) {
+Object.keys(handlerToEvent).forEach(function(i) {
 	privateProps[i] = true;
 });
 
-function updateHammer (hammer, props) {
+function updateHammer(hammer, props) {
 	if (props.hasOwnProperty('vertical')) {
 		console.warn('vertical is deprecated, please use `direction` instead');
 	}
 
 	var directionProp = props.direction;
 	if (directionProp || props.hasOwnProperty('vertical')) {
-		var direction = directionProp ? directionProp : (props.vertical ? 'DIRECTION_ALL' : 'DIRECTION_HORIZONTAL');
+		var direction = directionProp
+			? directionProp
+			: props.vertical ? 'DIRECTION_ALL' : 'DIRECTION_HORIZONTAL';
 		hammer.get('pan').set({ direction: Hammer[direction] });
 		hammer.get('swipe').set({ direction: Hammer[direction] });
 	}
 
 	if (props.options) {
-		Object.keys(props.options).forEach(function (option) {
+		Object.keys(props.options).forEach(function(option) {
 			if (option === 'recognizers') {
-				Object.keys(props.options.recognizers).forEach(function (gesture) {
+				Object.keys(props.options.recognizers).forEach(function(gesture) {
 					var recognizer = hammer.get(gesture);
 					recognizer.set(props.options.recognizers[gesture]);
 					if (props.options.recognizers[gesture].requireFailure) {
-						recognizer.requireFailure(props.options.recognizers[gesture].requireFailure);
+						recognizer.requireFailure(
+							props.options.recognizers[gesture].requireFailure
+						);
 					}
 				}, this);
 			} else {
@@ -83,13 +87,13 @@ function updateHammer (hammer, props) {
 	}
 
 	if (props.recognizeWith) {
-		Object.keys(props.recognizeWith).forEach(function (gesture) {
+		Object.keys(props.recognizeWith).forEach(function(gesture) {
 			var recognizer = hammer.get(gesture);
 			recognizer.recognizeWith(props.recognizeWith[gesture]);
 		}, this);
 	}
 
-	Object.keys(props).forEach(function (p) {
+	Object.keys(props).forEach(function(p) {
 		var e = handlerToEvent[p];
 		if (e) {
 			hammer.off(e);
@@ -99,24 +103,24 @@ function updateHammer (hammer, props) {
 }
 
 export default class HammerComponent extends React.Component {
-    static displayName = 'Hammer';
+	static displayName = 'Hammer';
 
-    static propTypes = {
+	static propTypes = {
 		className: PropTypes.string,
 	};
 
-    componentDidMount() {
+	componentDidMount() {
 		this.hammer = new Hammer(this.domElement);
 		updateHammer(this.hammer, this.props);
 	}
 
-    componentDidUpdate() {
+	componentDidUpdate() {
 		if (this.hammer) {
 			updateHammer(this.hammer, this.props);
 		}
 	}
 
-    componentWillUnmount() {
+	componentWillUnmount() {
 		if (this.hammer) {
 			this.hammer.stop();
 			this.hammer.destroy();
@@ -124,10 +128,10 @@ export default class HammerComponent extends React.Component {
 		this.hammer = null;
 	}
 
-    render() {
+	render() {
 		var props = {};
 
-		Object.keys(this.props).forEach(function (i) {
+		Object.keys(this.props).forEach(function(i) {
 			if (!privateProps[i]) {
 				props[i] = this.props[i];
 			}
